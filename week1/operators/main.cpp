0d9b70ac42678548ec69ceaeac0e5251229bea18
Mayank Tiwari <tmayank723@gmail.com>
2024-05-20 23:29:43 +0530

    Week 1: Operators in C++

---
+++ week1/operators/main.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    // int a = 5;
+    // cout << a << endl;
+    // cout << (++a) << endl;
+    // // a ki value 6 ho chuki h
+    // cout << (a++) << endl;
+    // // print kara toh 6 hi print hoga but a ki value 7 ho chuki hogi.
+    // cout << a << endl;
+    // // mtlb yaha tk a ki value 7 ban chuki hai 
+    // cout << (--a) << endl;
+    // // a ki value 6 ban chuki hai 
+    // cout << (a--) << endl;
+    // // Print to 6 hi hoga but a ki value 5 ban chuki hogi.
+    // // Let's verify
+    // cout << a << endl;
+
+
+    // Arithematic Operator
+    // int a = 10;
+    // int b = 5;
+    // cout << (a+b) << endl;
+    // cout << (a-b) << endl;
+    // cout << (a*b) << endl;
+    // cout << (a/b) << endl;
+    // cout << (a%b) << endl;
+    // cout << (5/2) << endl;
+
+    // Relational Operators
+    // cout << (10 >= 5) << endl;
+    // cout << (10 <= 5) << endl;
+    // cout << (10 == 5) << endl;
+
+    // Logical Operators
+    // bool cond1 = (10 >= 5);
+    // bool cond2 = (5>=5);
+    // bool cond3 = (2!=2);
+
+    // AND (&&) Operators
+    // if(cond1 && cond2 && cond3) {   
+    //     cout << "All conditions are true " << endl;
+    // } else {
+    //     cout << "All conditions are not true" << endl;
+    // }
+
+    // OR (||) Operators
+    // if(cond1 || cond2 || cond3) {
+    //     cout << "Atleast One condition true " << endl;
+    // } else {
+    //     cout << "All conditions are  false" << endl;
+    // }
+
+    // Not Operators
+    // bool condition = (5==10);
+    // cout << !condition << endl;
+
+
+    // Assignment Operators
+    // int arr = 5;
+    // cout << arr << endl;
+
+    // // arr = arr + 10;
+    // // arr += 10;
+
+    // // arr = arr * 10;
+    // arr *= 10;
+    // cout << arr << endl;
+
+    
+    // Bitwise Operators
+    // cout << (5&4) << endl;               // Bitwise AND(&) Operators
+    // cout << (5|4) << endl;               // Bitwise OR(|) Operators
+    // cout << (~1) << endl;                // BItwise Not , tilda (~) Operators (Flip all the bits)
+    // cout << (5 << 5) << endl;            // BItwise LeftShift (<<) Operators (multiple with 2 to the power)
+    // cout << (100 >> 2) << endl;          // BItwise RightShift (>>) Operators (divide with 2 to the power)
+    // cout << (5 ^ 5) << endl;             // Bitwise XOR Operator (same always gives 0) 
+    // cout << (5 ^ 4) << endl;             // Bitwise XOR Operator (different always gives 1) 
+
+    return 0;
+}
